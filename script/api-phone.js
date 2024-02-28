b9e0860811cce8195c33ac22802d00131184989a
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-28 17:00:20 +0600

    phone project done

--- script/api-phone.js
+++ script/api-phone.js
@@ -1,14 +1,14 @@
 
 
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhone(phones);
+    displayPhone(phones, isShowAll);
 }
 
 
-const displayPhone = phones => {
+const displayPhone = (phones, isShowAll) => {
     // console.log(phones);
     const displayCardContainer = document.getElementById('displayCardContainer');
 
@@ -18,15 +18,23 @@ const displayPhone = phones => {
     // show all btn hidden code
     const ShowAllBtnSection = document.getElementById('ShowAllBtnSection');
 
-    if (phones.length > 5) {
+
+    if (phones.length > 5 && !isShowAll) {
         ShowAllBtnSection.classList.remove('hidden');
     } else {
         ShowAllBtnSection.classList.add('hidden');
     }
 
+    // 2nd paramitar passi kina checking 
+    // console.log('is show all click', isShowAll);
+
+
+    if (!isShowAll) {
+        phones = phones.slice(0, 5);
+    }
 
     // display only 5 phones
-    phones = phones.slice(0, 5);
+    // phones = phones.slice(0, 5);
 
     phones.forEach(phone => {
         // console.log(phone);
@@ -45,7 +53,7 @@ const displayPhone = phones => {
                             <p>${phone.phone_name}</p>
                         </div>
                         <div class="card-actions">
-                            <button class="btn btn-primary">Show Details</button>
+                            <button onclick="handleShowDetails('${phone.slug}'); my_modal_1.showModal()" class="btn btn-primary">Show Details </button>
                         </div>
                     </div>
                 `
@@ -58,13 +66,13 @@ const displayPhone = phones => {
 
 } // displayPhone()
 
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
     // add loading spiner 
     toggleLoadingSpinner(true);
 
     // console.log('object');
     const inputField = document.getElementById('inputField').value;
-    loadPhone(inputField);
+    loadPhone(inputField, isShowAll);
 }
 
 
@@ -79,6 +87,45 @@ const toggleLoadingSpinner = (isLoading) => {
 }
 
 
+const handleShowAll = (isShowAll) => {
+    const showAllBtn = document.getElementById('showAllBtn');
+    handleSearch(true);
+
+}
+
+
+
+
+const handleShowDetails = async (phoneId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`);
+    const data = await res.json();
+    const phoneDetails = data.data;
+    showPhoneDetails(phoneDetails);
+}
+
+const showPhoneDetails = (phoneDetailsDataTaken) => {
+    console.log(phoneDetailsDataTaken);
+
+    const showDetailsContainer = document.getElementById('showDetailsContainer');
+    showDetailsContainer.innerHTML =
+
+        `
+                        <div class="text-center mb-7 flex justify-center">
+                            <img src="${phoneDetailsDataTaken.image}" alt="img">
+                        </div>
+                        <div>
+                            <h3 class="font-bold text-lg">${phoneDetailsDataTaken.name}</h3>
+                            <p class="py-4">It is a long established fact that a reader will be distracted by the readable
+                                content of a page when looking at its layout.</p>
+                            <p><span class="font-extrabold">Storage :</span> ${phoneDetailsDataTaken.mainFeatures.storage}</p>
+                            <p><span class="font-extrabold">Display Size :</span> ${phoneDetailsDataTaken.mainFeatures.displaySize}</p>
+                            <p><span class="font-extrabold">Chipset :</span> ${phoneDetailsDataTaken.mainFeatures.chipSet}</p>
+                            <p><span class="font-extrabold">Slug :</span> ${phoneDetailsDataTaken.slug}</p>
+                            <p><span class="font-extrabold">Release data :</span> ${phoneDetailsDataTaken.releaseDate}</p>
+                            <p><span class="font-extrabold">Brand :</span> ${phoneDetailsDataTaken.brand}</p>
+                        </div>
+    
+    `
 
 
 
@@ -87,6 +134,15 @@ const toggleLoadingSpinner = (isLoading) => {
 
 
 
+
+
+
+
+
+
+    //show the modal
+    my_modal_1.showModal();
+}
 
 
 
